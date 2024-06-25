d85f5b988b8284502ec00fa19499c7fcdc26de2b
SAMUEL_FILIPE_VIANA <22201394@OMEGAB41.BARROCA>
2024-06-25 08:11:24 -0300

    implementaçao do JSON da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import './App.css'
+import { useEffect, useState } from "react"
+
+
 
 function App() {
+  const [name, setName] = useState([]);
+  const [year, setYear] = useState([]);
+  const [season, setSeason] = useState([]);
+  const [medal, setMedal] = useState([]);
+
+  useEffect(() => {
+    fetch('./olympics.json')
+    .then(res => res.json)
+    .then(json => {
+      setMedal(json.sort((a, b) => a.name))
+    })
+  })
+
+
+
+
   return (
     <>
       
