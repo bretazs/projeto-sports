128b0e1585caa7680733c9346c1c6fc1ee256a1a
Samuu <samuelfilipevianadois@gmail.com>
2024-06-26 18:51:12 -0300

    Termino da implementacao da API de medalhistas de ouro das olimpiadas

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,57 @@ import { useEffect, useState } from "react"
 
 
 function App() {
-  const [name, setName] = useState([]);
-  const [year, setYear] = useState([]);
-  const [season, setSeason] = useState([]);
-  const [medal, setMedal] = useState([]);
+  const [names, setNames] = useState([]);
+  const [years, setYears] = useState([]);
+  const [medals, setMedals] = useState([]);
+
 
   useEffect(() => {
-    fetch('./olympics.json')
-    .then(res => res.json)
-    .then(json => {
-      setMedal(json.sort((a, b) => a.name))
-    })
-  })
+    fetch('./gold_medalists.json')
+      .then((res) => res.json())
+      .then((data) => {
+        const names = data.map(item => item.Name);
+        const years = data.map(item => item.Year);
+        const medals = data.map(item => item.Medal);
 
-
+        setNames(names)
+        setYears(years)
+        setMedals(medals)
+      })
+      .catch((error) => {
+        console.error('Erro ao carregar os dados: ', error)
+      });
+  }, []);
 
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Olympics Data from JSON</h1>
+      <div>
+        <h2>Names</h2>
+        <ul>
+          {names.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Years</h2>
+        <ul>
+          {years.map((year, index) => (
+            <li key={index}>{year}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Medals</h2>
+        <ul>
+          {medals.map((medal, index) => (
+            <li key={index}>{medal}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
