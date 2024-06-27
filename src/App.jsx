bce7b9577e533dc324c25477df8f13cda0db552b
Pedro Bretas <12302848@aluno.cotemig.com.br>
2024-06-27 20:01:52 -0300

    inicio html/css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react"
-
-
+import { useEffect, useState } from "react";
+import './style.css';
 
 function App() {
   const [names, setNames] = useState([]);
   const [years, setYears] = useState([]);
   const [medals, setMedals] = useState([]);
-
+  const [showNames, setShowNames] = useState(false); 
+  const [showYears, setShowYears] = useState(false); 
+  const [showMedals, setShowMedals] = useState(false); 
 
   useEffect(() => {
     fetch('./gold_medalists.json')
@@ -16,45 +17,50 @@ function App() {
         const years = data.map(item => item.Year);
         const medals = data.map(item => item.Medal);
 
-        setNames(names)
-        setYears(years)
-        setMedals(medals)
+        setNames(names);
+        setYears(years);
+        setMedals(medals);
       })
       .catch((error) => {
-        console.error('Erro ao carregar os dados: ', error)
+        console.error('Erro ao carregar os dados: ', error);
       });
   }, []);
 
-
   return (
-    <div>
-      <h1>Olympics Data from JSON</h1>
-      <div>
-        <h2>Names</h2>
-        <ul>
-          {names.map((name, index) => (
-            <li key={index}>{name}</li>
-          ))}
-        </ul>
+    <div className="container">
+      <h1 className="heading">Olympics Data from JSON</h1>
+      <div className="category">
+        <h2 className="names" onClick={() => setShowNames(!showNames)}>Names</h2>
+        {showNames && (
+          <ul>
+            {names.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))}
+          </ul>
+        )}
       </div>
-      <div>
-        <h2>Years</h2>
-        <ul>
-          {years.map((year, index) => (
-            <li key={index}>{year}</li>
-          ))}
-        </ul>
+      <div className="category">
+        <h2 className="years" onClick={() => setShowYears(!showYears)}>Years</h2>
+        {showYears && (
+          <ul>
+            {years.map((year, index) => (
+              <li key={index}>{year}</li>
+            ))}
+          </ul>
+        )}
       </div>
-      <div>
-        <h2>Medals</h2>
-        <ul>
-          {medals.map((medal, index) => (
-            <li key={index}>{medal}</li>
-          ))}
-        </ul>
+      <div className="category">
+        <h2 className="medals" onClick={() => setShowMedals(!showMedals)}>Medals</h2>
+        {showMedals && (
+          <ul>
+            {medals.map((medal, index) => (
+              <li key={index}>{medal}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
